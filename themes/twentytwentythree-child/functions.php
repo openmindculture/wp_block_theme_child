6fa7ba515fa837ad8f321b39244a40a858a8fcc4
Ingo Steinke <ingosteinke@gmx.de>
2023-08-17 12:05:24 +0200

    remove redundant actions

--- themes/twentytwentythree-child/functions.php
+++ themes/twentytwentythree-child/functions.php
@@ -9,34 +9,3 @@ function my_child_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 
-/*
- * Registering your colors
- * In your theme’s functions.php, add support for the custom color palette by specifying the following for each color option:
- *
- * Name: visible to user
- * Slug: used in CSS class
- * Color: used for rendering elements in the Gutenberg block editor
- */
-
-add_theme_support( 'editor-color-palette', array(
-	array(
-		'name'  => __( 'Blue', 'twentytwentythree-child' ),
-		'slug'  => 'blue',
-		'color'	=> '#59BACC',
-	),
-	array(
-		'name'  => __( 'Green', 'twentytwentythree-child' ),
-		'slug'  => 'green',
-		'color' => '#58AD69',
-	),
-	array(
-		'name'  => __( 'Orange', 'twentytwentythree-child' ),
-		'slug'  => 'orange',
-		'color' => '#FFBC49',
-	),
-	array(
-		'name'	=> __( 'Red', 'twentytwentythree-child' ),
-		'slug'	=> 'red',
-		'color'	=> '#E2574C',
-	),
-) );
