9766180556891d7a1a1851d949ac63d409e656ea
Ingo Steinke <ingosteinke@gmx.de>
2025-09-25 13:43:24 +0200

    fix include paths for child theme

--- themes/twentytwentyfive-child/functions.php
+++ themes/twentytwentyfive-child/functions.php
@@ -8,12 +8,12 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-require_once( get_template_directory() . '/inc/functions/customize-frontend.php' );
+require_once( get_stylesheet_directory() . '/inc/functions/customize-frontend.php' );
 
 add_action( 'wp_enqueue_scripts', function() {
 	wp_enqueue_style( 'twentytwentyfive-child-style', get_stylesheet_uri() );
 } );
 
 if (is_admin()) {
-    require_once( get_template_directory() . '/inc/functions/customize-backend.php' );
+    require_once( get_stylesheet_directory() . '/inc/functions/customize-backend.php' );
 }
