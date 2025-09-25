96d3089c6000ee92fba48207db0a383836a6810e
Ingo Steinke <ingosteinke@gmx.de>
2025-09-25 16:58:21 +0200

    remove obsolete stuff

--- themes/twentytwentythree-child/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package twentytwentythree-child
- * @author openmindculture
- */
-
-add_action( 'wp_enqueue_scripts', function() {
-	wp_enqueue_style( 'twentytwentythree-child-style', get_stylesheet_uri() );
-} );
