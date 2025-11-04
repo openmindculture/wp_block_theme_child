54aca7ea83e20ee1b9d7748524fd1e3590d7c70b
Ingo Steinke <ingosteinke@ingosteinke.de>
2025-11-04 19:53:51 +0100

    fix and improve header, footer, site layout

--- themes/twentytwentyfive-child/header.php
+++ themes/twentytwentyfive-child/header.php
@@ -5,14 +5,31 @@
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<header>CUSTOM HEADER
-    <nav>
-		<?php
-		wp_nav_menu(array(
-			'theme_location' => 'primary',
-			'fallback_cb' => false,
-			'container' => false,
-		));
-		?>
-    </nav>
+<header id="masthead" class="site-header">
+    <div class="site-header-inner wp-block-group alignfull"><!-- wp:group {"layout":{"type":"constrained"}} -->
+        <div class="site-header-row wp-block-group">
+            <div class="site-branding wp-block-group alignwide">
+                <div class="wrap">
+                    <a href="<?php
+					echo esc_url(home_url('/'));
+					?>" rel="home" class="site-title-link">
+                        <picture>
+                            <img src="<?php echo get_stylesheet_directory_uri() ?>/img/logo.png" alt="home logo" class="site-logo" width="640" height="289">
+                        </picture>
+                    </a>
+                </div>
+            </div>
+            <div class="site-navigation wp-block-group alignwide">
+                <nav>
+					<?php
+					wp_nav_menu(array(
+						'theme_location' => 'primary',
+						'fallback_cb' => false,
+						'container' => false,
+					));
+					?>
+                </nav>
+            </div>
+        </div>
+    </div>
 </header>
--- themes/twentytwentyfive-child/index.php
+++ themes/twentytwentyfive-child/index.php
@@ -1,11 +1,16 @@
 <?php // fallback code for classic/hybrid theme behavior; remove this file for full-site editing
 get_header();
 if (have_posts()):
-the_post();
-?>
-<div id="content" class="site-content">
-    <main class="site-main" id="site-main">
-        <?php the_content(); ?>
+	the_post();
+	?>
+    <main class="site-main wp-block-group" id="site-main">
+        <div class="wp-block-group alignfull">
+            <h1 class="site-main-title"><?php echo the_title() ?></h1>
+			<?php echo get_the_post_thumbnail( get_the_ID(), 'full' );  ?>
+            <div class="custom-content-wrapper alignfull is-layout-constrained">
+				<?php the_content(); ?>
+            </div>
+        </div>
     </main>
 <?php endif; /* have_posts */
 get_footer();
