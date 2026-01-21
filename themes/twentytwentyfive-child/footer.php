20014ce77a21fede612b3e9d8658a85a58ce742d
Ingo Steinke <ingosteinke@ingosteinke.de>
2026-01-21 08:28:32 +0100

    fotter with social sharing links

--- themes/twentytwentyfive-child/footer.php
+++ themes/twentytwentyfive-child/footer.php
@@ -2,14 +2,21 @@
     <div class="site-footer-inner wp-block-group alignwide">
         <div class="wp-block-group alignfull">
             <div class="wp-block-columns is-layout-flex">
-                <span class="footer-copyright">&copy; <?php echo date("Y"); ?></span>
-                <div class="wp-block-column">
-                    <nav class="is-horizontal wp-block-navigation is-layout-flex  wp-block-navigation-is-layout-flex" aria-label="Footer-Menü"><ul class="wp-block-navigation__container  is-vertical wp-block-navigation"><li class=" wp-block-navigation-item wp-block-navigation-link"><a class="wp-block-navigation-item__content" href="/impressum/"><span class="wp-block-navigation-item__label">Impressum</span></a></li><li class=" wp-block-navigation-item wp-block-navigation-link"><a class="wp-block-navigation-item__content" href="/datenschutzerklaerung/"><span class="wp-block-navigation-item__label">Datenschutzerklärung</span></a></li></ul></nav>
+                <div class="wp-block-column footer-links">
+                    <a href="/impressum/">Impressum</a>
+                    <a href="/datenschutzerklaerung/">Datenschutz</a>
+                    <a href="#teilen">Teilen</a>
+                    <div class="initially-hidden" id="teilen">
+                        <a href="https://www.linkedin.com/sharing/share-offsite/?url=https://www.example.com" target="_blank" rel="noopener">LinkedIn</a>
+                        <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.example.com" target="_blank" rel="noopener">Facebook</a>
+                        <a class="initially-hidden copy-link-location" href="#">Link kopieren</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 	<?php wp_footer(); ?>
 </footer>
+</div><?php /* /div#page.site opened in header.php */ ?>
 </body>
 </html>
--- themes/twentytwentyfive-child/js/scripts.js
+++ themes/twentytwentyfive-child/js/scripts.js
@@ -1,36 +1,51 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var themeMenuOpener = document.getElementById('site-navigation-menu-link');
-  if (themeMenuOpener) {
-    themeMenuOpener.addEventListener('click', function (event) {
-      event.preventDefault();
-      var themeNavigationMenu = document.getElementById('navigation-menu');
-      if (themeNavigationMenu && themeNavigationMenu.classList) {
-        themeNavigationMenu.classList.add('active');
-        var themeNavigationWrapper = document.getElementById('navigation-menu');
-        if (themeNavigationWrapper) {
-          themeNavigationWrapper.classList.add('active');
-        }
-      }
-      document.body.classList.add('is-menu-open');
-    });
-  }
+document.addEventListener('DOMContentLoaded', function () {
+    var themeMenuOpener = document.getElementById('site-navigation-menu-link');
+    if (themeMenuOpener) {
+        themeMenuOpener.addEventListener('click', function (event) {
+            event.preventDefault();
+            var themeNavigationMenu = document.getElementById('navigation-menu');
+            if (themeNavigationMenu && themeNavigationMenu.classList) {
+                themeNavigationMenu.classList.add('active');
+                var themeNavigationWrapper = document.getElementById('navigation-menu');
+                if (themeNavigationWrapper) {
+                    themeNavigationWrapper.classList.add('active');
+                }
+            }
+            document.body.classList.add('is-menu-open');
+        });
+    }
 
-  var themeMenuCloser = document.getElementById('site-navigation-menu-close');
-  if (themeMenuCloser) {
-    themeMenuCloser.addEventListener('click', function (event) {
-      event.preventDefault();
-      event.stopPropagation();
-      var themeNavigationMenu = document.getElementById('navigation-menu');
-      if (themeNavigationMenu && themeNavigationMenu.classList) {
-        themeNavigationMenu.classList.remove('active');
-        var themeNavigationWrapper = document.getElementById('navigation-menu');
-        if (themeNavigationWrapper) {
-          themeNavigationWrapper.classList.remove('active');
+    var themeMenuCloser = document.getElementById('site-navigation-menu-close');
+    if (themeMenuCloser) {
+        themeMenuCloser.addEventListener('click', function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            var themeNavigationMenu = document.getElementById('navigation-menu');
+            if (themeNavigationMenu && themeNavigationMenu.classList) {
+                themeNavigationMenu.classList.remove('active');
+                var themeNavigationWrapper = document.getElementById('navigation-menu');
+                if (themeNavigationWrapper) {
+                    themeNavigationWrapper.classList.remove('active');
+                }
+            }
+            document.body.classList.remove('is-menu-open');
+        });
+    }
+
+    if (navigator.clipboard && location.origin) {
+        const copyTriggers = document.querySelectorAll('.copy-link-location');
+        console.log(copyTriggers);
+        for (const trigger of copyTriggers) {
+            console.log(trigger);
+            trigger.addEventListener('click', async (e) => {
+                e.preventDefault();
+                await navigator.clipboard.writeText(location.origin + location.pathname);
+                alert('Die Web-Adresse wurde in die Zwischenabglage kopiert.');
+            });
+            trigger.classList.remove('initially-hidden');
         }
-      }
-      document.body.classList.remove('is-menu-open');
-    });
-  }
-  /* ensure indicating JS support, should be done by WordPress core, TODO remove when obsolete */
-  document.documentElement.className = document.documentElement.className.replace('no-js','js');
+    }
+
+    /* ensure indicating JS support, should be done by WordPress core, TODO remove when obsolete */
+    document.documentElement.className = document.documentElement.className.replace('no-js', 'js');
 });
